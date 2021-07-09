c98ada9beb87e1bdd5a3604a20c370bf537ff11a
Davidhdm <david.hdm9@gmail.com>
2021-07-09 12:56:33 +0200

    writer profile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,5 +79,6 @@
             @yield('content')
         </main>
     </div>
+    <script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
 </body>
 </html>
--- resources/views/profileViews/writerProfile.blade.php
+++ resources/views/profileViews/writerProfile.blade.php
@@ -1,23 +1,93 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+  <section class="writerProfile">
+    <div class="writerProfilePic">
+      <figure>
+        <img
+          src="https://cultivatedculture.com/wp-content/uploads/2019/12/LinkedIn-Profile-Picture-Example-Sami-Viitama%CC%88ki-.jpeg"
+          alt="">
+      </figure>
+      <span class="writerName">Writer's Name</span>
     </div>
-</div>
+    <p class="writerDesc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+      the industry's standard dummy text ever since the 1500s.</p>
+  </section>
+  <article class="writerSample">
+    <table class="table table-sm">
+      <thead>
+        <tr>
+          <th scope="col">Title</th>
+          <th scope="col">Genre</th>          
+          <th scope="col">Year</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Example Book 1</td>
+          <td>Comedy</td>          
+          <td>2015</td>
+        </tr>
+        <tr>
+          <td>Example Book 2</td>
+          <td>Thriller</td>          
+          <td>2016</td>
+        </tr>
+        <tr>
+          <td>Example Book 3</td>
+          <td>Romance</td>          
+          <td>2019</td>
+        </tr>
+        <tr>
+          <td>Example Book 1</td>
+          <td>Comedy</td>          
+          <td>2015</td>
+        </tr>
+        <tr>
+          <td>Example Book 2</td>
+          <td>Thriller</td>          
+          <td>2016</td>
+        </tr>
+        <tr>
+          <td>Example Book 3</td>
+          <td>Romance</td>          
+          <td>2019</td>
+        </tr>
+        <tr>
+          <td>Example Book 1</td>
+          <td>Comedy</td>          
+          <td>2015</td>
+        </tr>
+        <tr>
+          <td>Example Book 2</td>
+          <td>Thriller</td>          
+          <td>2016</td>
+        </tr>
+        <tr>
+          <td>Example Book 3</td>
+          <td>Romance</td>          
+          <td>2019</td>
+        </tr>
+        <tr>
+          <td>Example Book 1</td>
+          <td>Comedy</td>          
+          <td>2015</td>
+        </tr>
+        <tr>
+          <td>Example Book 2</td>
+          <td>Thriller</td>          
+          <td>2016</td>
+        </tr>
+        <tr>
+          <td>Example Book 3</td>
+          <td>Romance</td>          
+          <td>2019</td>
+        </tr>
+      </tbody>
+    </table>
+  </article>
+  <div class="buttons">
+    <span class="iconify likeBtn" data-icon="cil:heart" data-inline="false"></span>
+    <span class="iconify crossBtn" data-icon="emojione-monotone:cross-mark" data-inline="false"></span>
+  </div>
 @endsection
