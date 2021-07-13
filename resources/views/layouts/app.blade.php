0842eeda75574b7b5e05cdb5a7c67b7ab7d72a46
Berta LI <you@example.com>
2021-07-13 15:16:25 +0200

    creacion componente header-user

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
   <!-- Scripts -->
   <script src="{{ asset('js/app.js') }}" defer></script>
+  @livewireScripts
 
   <!-- Fonts -->
   <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -19,6 +20,7 @@
 
   <!-- Styles -->
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  @livewireStyles
 
 </head>
 
---
+++ resources/views/livewire/header-user.blade.php
@@ -0,0 +1,3 @@
+<div>
+<h1>Hola, componente de livewire...</h1>
+</div>
