db5f5ee302e4fb9ce0f2d0b244c8b459333a343a
Davidhdm <david.hdm9@gmail.com>
2021-07-20 11:12:50 +0200

    el carajo del git

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,9 +17,12 @@
 <!-- Fonts -->
 <link rel="dns-prefetch" href="//fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 <!-- Styles -->
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<link href="{{ asset('css/home-index.css') }}" rel="stylesheet">
+
 @livewireStyles
 </head>
 
@@ -95,6 +98,7 @@
     $('#exampleModal').modal('hide');
   });
 </script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,29 +1,7 @@
 @extends('layouts.app')
-@section('title', __('Welcome'))
+{{-- @section('title', __('Welcome')) --}}
 @section('content')
   <div class="container-fluid">
-    <div class="row justify-content-center">
-      <div class="col-md-12">
-        <div class="card">
-          <div class="card-header">
-            <h5><span class="text-center fa fa-home"></span> @yield('title')</h5>
-          </div>
-          <div class="card-body">
-            <h5>
-              @guest
-
-                {{ __('Welcome to') }} {{ config('app.name', 'Laravel') }} !!! </br>
-                Please contact admin to get your Login Credentials or click "Login" to go to your Dashboard.
-
-              @else
-                Hi {{ Auth::user()->name }}, Welcome back to {{ config('app.name', 'Laravel') }}.
-                @endif
-              </h5>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
 
     <div
       class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0 color-body">
