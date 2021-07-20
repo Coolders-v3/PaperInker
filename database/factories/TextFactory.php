a1328e32c1f36b5b54a00f956e4e07661e0325a9
Davidhdm <david.hdm9@gmail.com>
2021-07-20 09:09:08 +0200

    merge

--- database/factories/TextFactory.php
+++ database/factories/TextFactory.php
@@ -8,21 +8,15 @@ use Illuminate\Support\Str;
 
 class TextFactory extends Factory
 {
-    protected $model = Text::class;
+  protected $model = Text::class;
 
-    public function definition()
-    {
-        return [
-			'genre' => $this->faker->name(),
-			'title' => $this->faker->name(),
-			'description' => $this->faker->name(),
-<<<<<<< HEAD
-			'year' => $this->faker->year()
-      ];
-=======
-			'year' => $this->faker->year(),
-			
-        ];
->>>>>>> b2eec397459cda4c7d92eca0061b7b8d1f817670
-    }
+  public function definition()
+  {
+    return [
+      'genre' => $this->faker->name(),
+      'title' => $this->faker->name(),
+      'description' => $this->faker->name(),
+      'year' => $this->faker->year()
+    ];
+  }
 }
--- resources/views/livewire/texts/view.blade.php
+++ resources/views/livewire/texts/view.blade.php
@@ -1,8 +1,8 @@
 @section('title', __('Texts'))
-<div class="container-fluid">
+{{-- <div class="container-fluid">
 	<div class="row justify-content-center">
-		<div class="col-md-12">
-			<div class="card">
+		<div class="col-md-12"> --}}
+			{{-- <div class="card"> --}}
 				<div class="card-header">
 					<div style="display: flex; justify-content: space-between; align-items: center;">
 						<div class="float-left">
@@ -19,17 +19,17 @@
 					</div>
 				</div>
 				
-				<div class="card-body">
+				{{-- <div class="card-body"> --}}
 						@include('livewire.texts.create')
 						@include('livewire.texts.update')
-				<div class="table-responsive">
-					<table class="table table-bordered table-sm">
+				{{-- <div class="table-responsive"> --}}
+					<table class="table table-hover">
 						<thead class="thead">
 							<tr> 
 								<th>Title</th>
 								<th>Genre</th>
 								<th>Year</th>
-								<td>ACTIONS</td>
+								<td>&nbsp;</td>
 							</tr>
 						</thead>
 						<tbody>
@@ -38,7 +38,7 @@
 								<td>{{ $row->title }}</td>
 								<td>{{ $row->genre }}</td>
 								<td>{{ $row->year }}</td>
-								<td width="90">
+								<td width="10">
 								<div class="btn-group">
 									<button type="button" class="btn btn-info btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 									Actions
@@ -53,9 +53,9 @@
 						</tbody>
 					</table>						
 					{{ $texts->links() }}
-					</div>
-				</div>
-			</div>
+					{{-- </div> --}}
+				{{-- </div> --}}
+			{{-- </div>
 		</div>
 	</div>
-</div>
+</div> --}}
