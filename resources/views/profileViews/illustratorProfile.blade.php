bb29aebf6f494ae3985fe150e6b14ac9f6c2a6c6
Berta LI <you@example.com>
2021-07-21 15:18:41 +0200

    actualización componentes

--- resources/views/profileViews/illustratorProfile.blade.php
+++ resources/views/profileViews/illustratorProfile.blade.php
@@ -1,10 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-<<<<<<< HEAD
-  
-  @livewire('read-illustrator')
-=======
   @livewire('view')
->>>>>>> 643ca31e4e321458b515b6db3f58bf735aacfe1c
 @endsection
