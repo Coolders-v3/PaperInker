d492dc71e593c03d285855d69d09979b07cd2b92
Berta LI <you@example.com>
2021-07-21 13:02:15 +0200

    importacion user-info para illustrations y texts

--- resources/views/profileViews/writerProfile.blade.php
+++ resources/views/profileViews/writerProfile.blade.php
@@ -2,30 +2,6 @@
 
 @section('content')
   @livewire('user-info')
-  <article class="samples">
-    <table class="table table-sm">
-      <thead>
-        <tr>
-          <th scope="col">Title</th>
-          <th scope="col">Genre</th>
-          <th scope="col">Year</th>
-        </tr>
-      </thead>
-      <tbody>
-        @foreach ($texts as $text)
-        <tr>
-          <td>{{ $text->title }}</td>
-          <td>{{ $text->jobGenre }}</td>          
-          <td>{{ $text->yearOfCreation }}</td>
-        </tr>
-       @endforeach
-      </tbody>
-    </table>
-  </article>
-  <div class="buttons">
-    <span class="iconify likeBtn" data-icon="cil:heart" data-inline="false"></span>
-    <span class="iconify crossBtn" data-icon="emojione-monotone:cross-mark" data-inline="false"></span>
-  </div>
-
+  @livewire('read-writer')
 @endsection
-
+  
