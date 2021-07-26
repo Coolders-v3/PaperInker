05fe49cce6a0921e13fc6384a21a1c4dca83eb8c
Gabi Piñeiro <gabispineiro@gmail.com>
2021-07-26 14:29:28 +0200

    gabi rutas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\ReadWriter;
 use Illuminate\Support\Facades\Auth;
@@ -21,7 +22,11 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/illustrations',[App\Http\Controllers\HomeController::class, 'index'])->middleware('auth')->name('illustrations');
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 //Route Hooks - Do not delete//
 Route::view('illustrations', 'livewire.illustrations.index')->middleware('auth')->name('illustrations');
 Route::view('texts', 'livewire.texts.index')->middleware('auth')->name('texts');
+
+
