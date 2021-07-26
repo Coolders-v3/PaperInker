f48e963daf17e8831d8df03d0d69fa187e8ae684
Davidhdm <david.hdm9@gmail.com>
2021-07-26 09:42:07 +0200

    passing variables to components

--- app/Http/Livewire/UserSlides.php
+++ app/Http/Livewire/UserSlides.php
@@ -5,26 +5,53 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\Writer;
+use App\Models\Illustrator;
 
 class UserSlides extends Component
 {
-  public function getProfile()
+  public $user;
+  public $userDescription;
+  public $userImg;
+
+  public function getRandomProfile()
   {
     $loggedUser = Auth::user();
     $users = User::all();
+    $writers = Writer::all();
+    $illustrators = Illustrator::all();
 
     if ($loggedUser->isWriter) {
-      return $users->where('isWriter', false)->random(1);
+      $this->user = $users->where('isWriter', false)
+        ->random(1)[0];
+
+      $this->userDescription = $illustrators->where('user_id', $this->user->id)[0]
+        ->personaldescription;
+
+      $this->userImg = $illustrators->where('user_id', $this->user->id)[0]
+        ->personalImage;
     }
 
     if (!$loggedUser->isWriter) {
-      return $users->where('isWriter', true)->random(1);
+      $this->user = $users->where('isWriter', true)
+        ->random(1)[0];
+
+      $this->userDescription = $writers->where('user_id', $this->user->id)[0]
+        ->personaldescription;
+
+      $this->userImg = $writers->where('user_id', $this->user->id)[0]
+        ->personalImage;
     }
   }
 
   public function render()
   {
-    $user = $this->getProfile();
-    return view('livewire.user.user-slides', $user);
+    $this->getRandomProfile();
+
+    return view('livewire.user.user-slides', [
+      'user' => $this->user,
+      'userDescription' => $this->userDescription,
+      'userImg' => $this->userImg
+    ]);
   }
 }
--- resources/views/livewire/user/user-slides.blade.php
+++ resources/views/livewire/user/user-slides.blade.php
@@ -1,19 +1,31 @@
 @extends('layouts.app')
 @section('content')
-  {{-- @livewire('user') --}}
+  <div>
+    <section class="profile">
+      <div class="profilePic">
+        <figure>
+          <img src="{{ $userImg }}" alt="">
+        </figure>
+        <span class="name">{{ $user->name }}</span>
+      </div>
+      <p class="description">
+        {{ $userDescription }}
+      </p>
+    </section>
+  </div>
   <section>
     @if (Auth::user()->isWriter)
-      
+
     @endif
 
     @if (!Auth::user()->isWriter)
-    <div class="writerSample">
-      <p>
-        Mei eu mollis albucius, ex nisl contentiones vix. Duo persius volutpat at, cu iuvaret epicuri mei. Duo posse
-        pertinacia no, ex dolor contentiones mea. Nec omnium utamur dignissim ne. Mundi lucilius salutandi an sea, ne
-        sea aeque iudico comprehensam. Populo delicatissimi ad pri. Ex vitae accusam vivendum pro.
-      </p>
-    </div>
+      <div class="writerSample">
+        <p>
+          Mei eu mollis albucius, ex nisl contentiones vix. Duo persius volutpat at, cu iuvaret epicuri mei. Duo posse
+          pertinacia no, ex dolor contentiones mea. Nec omnium utamur dignissim ne. Mundi lucilius salutandi an sea, ne
+          sea aeque iudico comprehensam. Populo delicatissimi ad pri. Ex vitae accusam vivendum pro.
+        </p>
+      </div>
     @endif
   </section>
   <div class="slideButtons">
@@ -21,4 +33,3 @@
     <span class="iconify nextBtn" data-icon="bi:x-lg" data-inline="false"></span>
   </div>
 @endsection
-
