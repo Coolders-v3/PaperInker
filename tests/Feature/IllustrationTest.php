30b62fdf4f85a7b758c62eb0e10e510029438742
unknown <andreabcm16@gmail.com>
2021-07-28 21:47:11 +0200

    test illustrations

---
+++ tests/Feature/IllustrationTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Illustration;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class IllustrationTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_viewIllustrations()
+    {
+       /*  $this->withoutExceptionHandling(); */
+        Illustration::factory(2)->create();
+
+        $response = $this->get('/illustrations');
+
+        $response->assertStatus(200);
+    }
+}
