3ad54f3c92bc45928b589313eddc12b8cde4f4f0
unknown <andreabcm16@gmail.com>
2021-07-28 10:52:18 +0200

    componente logo-user

---
+++ resources/views/livewire/user/logo-user.blade.php
@@ -0,0 +1,7 @@
+<div class="container-header-logo">
+    <a class="navbar-brand" href="{{ url('/') }}">
+        <img class="logo-user-component"
+        src="{{ asset('images/logo-user.png') }}" 
+        alt="logo-user">
+    </a>
+    </div>
